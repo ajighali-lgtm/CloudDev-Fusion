dbc1580086b3cd9e8cac82312b4386951cf84d60
ajighali-lgtm <aji.ghali@gmail.com>
2026-06-22 19:19:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php
-echo "Hello World!";​
-$serverName = getenv('SQL_SERVER_NAME'); // Get server name from environment variables​
-$connectionOptions = array(​
-    "Database" => getenv('SQL_DATABASE_NAME'), // Get database name from environment variables​
-    "Uid" => getenv('SQL_UID'),               // Get username from environment variables​
-    "PWD" => getenv('SQL_PWD')                // Get password from environment variables​
-);​
-try {​
-    $conn = new PDO("sqlsrv:server=$serverName;Database=".$connectionOptions['Database'], $connectionOptions['Uid'], $connectionOptions['PWD']);​
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);​
-    echo "Connected!";​
-} catch (PDOException $e) {​
-    echo "Connection failed: " . $e->getMessage();​
+
+echo "Hello World!";
+
+$serverName = getenv('SQL_SERVER_NAME'); // Get server name from environment variables
+$connectionOptions = array(
+    "Database" => getenv('SQL_DATABASE_NAME'), // Get database name from environment variables
+    "Uid" => getenv('SQL_UID'),               // Get username from environment variables
+    "PWD" => getenv('SQL_PWD')                // Get password from environment variables
+);
+
+try {
+    $conn = new PDO("sqlsrv:server=$serverName;Database=".$connectionOptions['Database'], $connectionOptions['Uid'], $connectionOptions['PWD']);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected!";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
