b0703e1e0de2aa7a7c15eabb5a91da4c534a3f3d
ajighali-lgtm <aji.ghali@gmail.com>
2026-06-22 19:14:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,14 @@
 echo "Hello World!";​
-
-​
 $serverName = getenv('SQL_SERVER_NAME'); // Get server name from environment variables​
-
 $connectionOptions = array(​
-
     "Database" => getenv('SQL_DATABASE_NAME'), // Get database name from environment variables​
-
     "Uid" => getenv('SQL_UID'),               // Get username from environment variables​
-
     "PWD" => getenv('SQL_PWD')                // Get password from environment variables​
-
 );​
-
-​
 try {​
-
     $conn = new PDO("sqlsrv:server=$serverName;Database=".$connectionOptions['Database'], $connectionOptions['Uid'], $connectionOptions['PWD']);​
-
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);​
-
     echo "Connected!";​
-
 } catch (PDOException $e) {​
-
     echo "Connection failed: " . $e->getMessage();​
-
 }
